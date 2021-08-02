8ffd71e8305c656792a778442c213c5ea5a1004c
Andrew Klotz <agc.klotz@gmail.com>
2021-08-02 17:47:18 +0000

    use a board for new moves
    Signed-off-by: Andrew Klotz <agc.klotz@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,20 @@ mod logs;
 fn main() {
     logs::setup().expect("unable to configure logger");
 
+    let mut board = BoardModel::new();
+
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
         match line {
             Err(_) => break,
-            Ok(line) => handle_line(line),
+            Ok(line) => board = handle_line(board, line),
         }
     }
 }
 
-fn handle_line(line: String) {
+fn handle_line(board: BoardModel, line: String) -> BoardModel {
+    let command: Vec<&str> = line.split_whitespace().collect();
+
     if line == "uci" {
         write_msg("id name quick-chess");
         write_msg("id author Andrew Klotz");
@@ -24,15 +28,41 @@ fn handle_line(line: String) {
         write_msg("readyok")
     } else if line == "ucinewgame" {
         // game started with empty board
-    } else if line == "position startpos" {
+    } else if command[0] == "position" {
+        // position startpos
+        // position startpos moves e2e3
+        if command.len() == 2 {
+            // first move
+            write_msg("bestmove e2e3")
+        } else {
+            let last_move = command[3];
+            let (next_board, next_move) = find_next_move(board, last_move);
+            return next_board;
+        }
         // position startpos moves e2e4
-        write_msg("bestmove e2e4")
+    } else if command[0] == "go" {
+        // go wtime 2000 btime 2000 movestogo 180
+        // time control info comes after last move
     } else if line == "stop" {
         // stop searching, is there a resume option?
     } else if line == "quit" {
         // exit the program
     } else {
         write_msg(&format!("unhandled msg: {}", line));
+    }
+    board
+}
+
+fn find_next_move(board: BoardModel, last_move: &str) -> (BoardModel, &str) {
+    (board, "e2e3")
+}
+
+#[derive(Copy, Clone)]
+struct BoardModel {}
+
+impl BoardModel {
+    pub fn new() -> BoardModel {
+        BoardModel {}
     }
 }
 
